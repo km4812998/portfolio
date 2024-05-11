a9a81cfb8269ffba13b2afc74d3d9cb82c26ddfa
Mintu Kumar <km4812998@gmail.com>
2024-05-11 16:28:03 +0530

    Add files via upload

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,24 +1,42 @@
-import './Contact.css';
+import React,{ useRef } from "react";
+import "./Contact.css";
+import emailjs from "@emailjs/browser";
 
-function Contact(){
-    return(
-        <div className='contact'>
-            <div className="contactheading">
-                <h1>Contact Me</h1>
-            </div>
-            <div className="content">
-                <h3>Please fill out the form to contact me</h3>
-            </div>
-            <form>
-                <input type="text"className='input1' placeholder='Your Name'/>
-                <br />
-                <input type="number" className='input2' placeholder='Your Number'/>
-                <br />
-                <textarea placeholder='Your Message' className='text' cols="50" rows="5"></textarea>
-                <br />
-                <button className='submitbtn'>Submit</button>
-            </form>
-        </div>
-    )
+const Contact=()=> {
+  const form = useRef();
+  const sendEmail = (e) => {
+  e.preventDefault();
+
+    emailjs.sendForm("service_hv5k1bb", "template_cjw5tpv", form.current, {publicKey:'ivFBQB27xWGxGYEUL'})
+      .then((result) => {
+        alert(result.text)
+          console.log(result.text);
+          e.target.reset();
+          console.log("Message sent")
+        },
+        (error) => {
+          console.log( error.text);
+        }
+      );
+    };
+  return (
+    <div className="contact">
+      <div className="contactheading">
+        <h1>Contact Me</h1>
+      </div>
+      <div className="content">
+        <h3>Please fill out the form to contact me</h3>
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" className="input1" placeholder="Your Name" name='user_name'/>
+        <br />
+        <input type="email" className="input2" placeholder="Your Email" name='user_email'/>
+        <br />
+        <textarea placeholder="Your Message" className="text" cols="50" rows="5" name="message"></textarea>
+        <br />
+        <input type="submit" value="Send" className="submitbtn"/>
+      </form>
+    </div>
+  );
 }
-export default Contact
+export default Contact;
